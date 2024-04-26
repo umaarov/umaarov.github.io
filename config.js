15da2e76d6da2033fb6094b475230063c9c88d51
Umarov Ismoiljon <hs.umarov21@gmail.com>
2024-04-26 09:56:29 +0500

    Added again

---
+++ config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyC38M2uFrG3FOnQ3RDsutWO5tVcC5u9UEE',
+  authDomain: 'goat-5eda2.firebaseapp.com',
+  databaseURL: 'https://goat-5eda2-default-rtdb.firebaseio.com',
+  projectId: 'goat-5eda2',
+  storageBucket: 'goat-5eda2.appspot.com',
+  messagingSenderId: '189954731887',
+  appId: '1:189954731887:web:2591d17a7f12f15272c29a',
+  measurementId: 'G-VXDSXLFZ9M',
+};
+
+export default firebaseConfig;
